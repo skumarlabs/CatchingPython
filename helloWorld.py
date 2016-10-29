5b8fdc397e24a71de51e9813850a2a224270a1f8
Surendra Kumar <sur365@gmail.com>
2016-10-29 17:16:33 +0530

    mtSleepB

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import math
+import sys
+import random
+print (sys.platform)
+print(2**100)
+x='spam!'
+print(x*8)
+print math.pi 
+print random.random()
+print random.choice([1, 2, 3, 4])
+print dir(x)
